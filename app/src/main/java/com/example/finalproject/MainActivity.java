47f9df33105fd35c3f58595fd578e2f685ec07b5
JDAV-0 <jdavid7@umbc.edu>
2021-05-07 21:27:33 -0400

    new note button and clickability

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b_newNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b_newNote = (Button) findViewById(R.id.newNoteButton);//get id of new note button "+"
+
+        b_newNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "newNote!", Toast.LENGTH_LONG).show();//display the text of button1
+                newNote();
+            }
+        });
+
+
+    }
+    public void newNote()
+    {
+        Intent intent = new Intent(this, NewNote.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/finalproject/NewNote.java
+++ app/src/main/java/com/example/finalproject/NewNote.java
@@ -2,7 +2,9 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class NewNote extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class NewNote extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_note);
+
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+
     }
 }
+
+

c3c2583fd242998b656a1cd567f76a6fb5d3dc85
blastedryan <31160332+blastedryan@users.noreply.github.com>
2021-05-14 23:29:23 -0400

    added creating timed notifications

--- app/src/main/java/com/example/finalproject/AddNoteActivity.java
+++ app/src/main/java/com/example/finalproject/AddNoteActivity.java
@@ -1,19 +1,36 @@
 package com.example.finalproject;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.DatePickerDialog;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class AddNoteActivity extends Activity implements View.OnClickListener {
 
     private Button addBtn;
     private EditText titleET, noteET, dateET, priorityET;
     DBManager dbManager;
+    private Calendar myCalendar = Calendar.getInstance();
+    private long delay = 0;
+    private Date datein;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,6 +47,31 @@ public class AddNoteActivity extends Activity implements View.OnClickListener {
 
         dbManager = new DBManager(this);
         dbManager.open();
+
+        dateET.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        myCalendar.set(Calendar.YEAR, year);
+                        myCalendar.set(Calendar.MONTH, monthOfYear);
+                        myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                        datein = myCalendar.getTime();
+                        Date currentTime = Calendar.getInstance().getTime();
+                        delay = datein.getTime() - currentTime.getTime();
+
+                    }
+                };
+                new DatePickerDialog(
+                        AddNoteActivity.this, date,
+                        myCalendar.get(Calendar.YEAR),
+                        myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)
+                ).show();
+            }
+        });
+
         addBtn.setOnClickListener(this);
     }
 
@@ -39,15 +81,39 @@ public class AddNoteActivity extends Activity implements View.OnClickListener {
             case R.id.addRecord:
                 String title = titleET.getText().toString();
                 String note = noteET.getText().toString();
-                String date = dateET.getText().toString();
+                String myFormat = "mm/dd/yy" ;
+                SimpleDateFormat sdf = new SimpleDateFormat(myFormat , Locale.getDefault ()) ;
+                String date = sdf.format(datein);
                 String priority = priorityET.getText().toString();
 
                 dbManager.insert(title, note, date, priority);
+
+                String NOTIFICATION_CHANNEL_ID = "10001";//switch for whatever priority chosen
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(
+                        AddNoteActivity.this, NOTIFICATION_CHANNEL_ID)
+                        .setContentTitle("Title")
+                        .setContentText("Body")
+                        .setSmallIcon(R.drawable.ic_launcher_foreground)
+                        .setAutoCancel(true)
+                        .setPriority(NotificationCompat.PRIORITY_MAX) //for old versions
+                        .setChannelId(NOTIFICATION_CHANNEL_ID);//for old versions
+
+                scheduleNotification(builder.build(), delay);
+
 
                 Intent main = new Intent(AddNoteActivity.this, ListActivity.class)
                         .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(main);
                 break;
         }
+    }
+    private void scheduleNotification(Notification notification, long delay) {
+        Intent notificationIntent = new Intent(this, NotificationUtil.class);
+        notificationIntent.putExtra(NotificationUtil.NOTIFICATION_ID, 1);//unique id
+        notificationIntent.putExtra(NotificationUtil.NOTIFICATION, notification);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        long futureInMillis = SystemClock.elapsedRealtime() + delay;
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureInMillis, pendingIntent);
     }
 }

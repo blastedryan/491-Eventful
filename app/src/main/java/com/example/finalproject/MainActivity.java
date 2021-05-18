01c6b8527603e9a31d16da17a9061baeb832f975
rho97 <70784068+PaulRoh97@users.noreply.github.com>
2021-05-18 17:41:20 -0400

    Update MainActivity.java
    Fixed main activity

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.finalproject;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
 
-import android.os.Bundle;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b_newNote;
+
+    String titles[], dates[], notes[], priorities[];
+    int icon = R.drawable.icon;
+
+    RecyclerView recyclerView;
+    //database things; judy
+    private NotesDatabase database;
+    private NotesDao dao;
+    private List<Notes> notesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        database = Room.databaseBuilder(getApplicationContext(),
+                NotesDatabase.class,
+                "notes").allowMainThreadQueries().build();
+        dao = database.dao();
+    }
+
+    public void newNote() {
+        Intent intent = new Intent(this, NewNote.class);
+        startActivity(intent);
     }
 }

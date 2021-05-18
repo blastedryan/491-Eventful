d91392386c4a3fd0ab7d6d663b87f06e45e3715c
rho97 <70784068+PaulRoh97@users.noreply.github.com>
2021-05-18 17:24:52 -0400

    Revert "Merge branch 'addingNotes' into LocalDatabase"
    This reverts commit ef69b45dba81dfe3cc86d697855e8658c1519ab8, reversing
    changes made to cdd35dd71f335d2c5a2db544c2eb1acf1b957961.

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -2,54 +2,13 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button b_newNote;
-
-<<<<<<< Updated upstream
-=======
-    String titles[], dates[], notes[], priorities[];
-    int icon = R.drawable.icon;
-
-    RecyclerView recyclerView;
-
-    //database things; judy
-    private NotesDatabase database;
-    private NotesDao dao;
-    private List<Notes> notesList;
-
->>>>>>> Stashed changes
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        b_newNote = (Button) findViewById(R.id.newNoteButton);//get id of new note button "+"
-
-        b_newNote.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                newNote();
-            }
-        });
-
-        database = Room.databaseBulder(getApplicationContext(),
-                NotesDatabase.class,
-                "notes").allowMainThreadQueries().build();
-        dao = database.dao();
-    }
-    public void newNote()
-    {
-        Intent intent = new Intent(this, NewNote.class);
-        startActivity(intent);
     }
 }

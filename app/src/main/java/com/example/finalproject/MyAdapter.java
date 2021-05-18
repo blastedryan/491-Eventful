6cfb643011aee3438f076e01cc600bc0a81334c9
trackstarc <cedrick.tanon.2018@gmail.com>
2021-05-18 03:40:23 -0400

    Implemented Clickable Notes
    Second Big Push! Notes are now clickable and lead to an edit notes page where the data can be changed (hopefully).

--- app/src/main/java/com/example/finalproject/MyAdapter.java
+++ app/src/main/java/com/example/finalproject/MyAdapter.java
@@ -1,6 +1,7 @@
 package com.example.finalproject;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
@@ -16,6 +18,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     int icon;
     Context context;
 
+    // note attributes
     public MyAdapter(Context ct, String titles[], String dates[], String notes[], String priorities[], int image) {
         context = ct;
         s1 = titles;
@@ -35,11 +38,26 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        // creating the notes code
         holder.myTitle.setText(s1[position]);
         holder.myDate.setText(s2[position]);
         holder.myNote.setText(s3[position]);
         holder.myPriority.setText(s4[position]);
         holder.myIcon.setImageResource(icon);
+
+        // clickable notes code
+        holder.cardLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, ClickActivity.class);
+                intent.putExtra("Title", s1[position]);
+                intent.putExtra("Date", s2[position]);
+                intent.putExtra("Note", s3[position]);
+                intent.putExtra("Priority", s4[position]);
+                intent.putExtra("Icon", icon);
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -51,7 +69,9 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
         TextView myTitle, myDate, myNote, myPriority;
         ImageView myIcon;
+        ConstraintLayout cardLayout;
 
+        // linkage to XML files
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             myTitle = itemView.findViewById(R.id.myTitle);
@@ -59,6 +79,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
             myNote = itemView.findViewById(R.id.myNote);
             myPriority = itemView.findViewById(R.id.myPriority);
             myIcon = itemView.findViewById(R.id.myIcon);
+            cardLayout = itemView.findViewById(R.id.cardLayout);
         }
     }
 }

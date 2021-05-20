2c1b9da97b6154e65c63256b751a546ab6e1c669
blastedryan <31160332+blastedryan@users.noreply.github.com>
2021-05-19 22:52:30 -0400

    remerged my code back in and cleaned up code

--- app/src/main/java/com/example/finalproject/AddNoteActivity.java
+++ app/src/main/java/com/example/finalproject/AddNoteActivity.java
@@ -1,6 +1,5 @@
 package com.example.finalproject;
 
-import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.DatePickerDialog;
 import android.app.ListActivity;
@@ -14,30 +13,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
 import androidx.room.Room;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
 import java.util.Locale;
 
 public class AddNoteActivity extends AppCompatActivity {
 
     private Button addBtn, dateBtn;
-    //private EditText titleET, noteET, dateET, priorityET;
     private EditText titleET, noteET, priorityET;
-
-    //DBManager dbManager;
     private Calendar myCalendar = Calendar.getInstance();
     private long delay = 0;
-
     private Date datein = Calendar.getInstance().getTime();
 
     /******   judy adding database connection (input)      ******/
@@ -59,16 +50,10 @@ public class AddNoteActivity extends AppCompatActivity {
         int count = database.dao().countNotes();
 
         titleET = findViewById(R.id.titleEditText);
-
         noteET = findViewById(R.id.noteEditText);
-
-        noteET = findViewById(R.id.noteEditText);
-
         priorityET = findViewById(R.id.numberEditText);
         addBtn = findViewById(R.id.addRecord);
         dateBtn = findViewById(R.id.dateButton);
-        //dbManager = new DBManager(this);
-        //dbManager.open();
 
         dateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -105,55 +90,58 @@ public class AddNoteActivity extends AppCompatActivity {
     }
 
    public void addClick(View v) {
-        switch (v.getId()) {
-            case R.id.addRecord:
-                String title = titleET.getText().toString();
-                String note = noteET.getText().toString();
-                String myFormat = "MM/dd/yy" ;
-                SimpleDateFormat sdf = new SimpleDateFormat(myFormat , Locale.getDefault ()) ;
-                String date = sdf.format(datein);
-                String priority = priorityET.getText().toString();
+       if (v.getId() == R.id.addRecord) {
+           String title = titleET.getText().toString();
+           String note = noteET.getText().toString();
+           String myFormat = "MM/dd/yy";
+           SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.getDefault());
+           String date = sdf.format(datein);
+           String priority = priorityET.getText().toString();
 
-                Notes newNoteObj = new Notes();
+           Notes newNoteObj = new Notes();
 
-                if(title.isEmpty())
-                    title = "New Note " + newNoteObj.getId();
+           if (title.isEmpty())
+               title = "New Note " + newNoteObj.getId();
 
-                newNoteObj.setTitle(title);
-                newNoteObj.setDate(date);
-                newNoteObj.setNote(note);
-                newNoteObj.setPriority(priority);
+           newNoteObj.setTitle(title);
+           newNoteObj.setDate(date);
+           newNoteObj.setNote(note);
+           newNoteObj.setPriority(priority);
 
-                database.dao().insert(newNoteObj);
-                //dbManager.insert(title, note, date, priority);
+           database.dao().insert(newNoteObj);
+           String NOTIFICATION_CHANNEL_ID;
+           //switch for whatever priority chosen
+           if (Integer.parseInt(priority) == 3) {//high
+               NOTIFICATION_CHANNEL_ID = "10001";
+           } else if (Integer.parseInt(priority) == 2) {//norm
+               NOTIFICATION_CHANNEL_ID = "10002";
+           } else {//low
+               NOTIFICATION_CHANNEL_ID = "10003";
+           }
+           NotificationCompat.Builder builder = new NotificationCompat.Builder(
+                   AddNoteActivity.this, NOTIFICATION_CHANNEL_ID)
+                   .setContentTitle(title)
+                   .setContentText(note)
+                   .setSmallIcon(R.drawable.ic_launcher_foreground)
+                   .setAutoCancel(true)
+                   .setPriority(NotificationCompat.PRIORITY_MAX) //for old versions
+                   .setChannelId(NOTIFICATION_CHANNEL_ID);//for old versions
 
-                String NOTIFICATION_CHANNEL_ID = "10001";//switch for whatever priority chosen
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(
-                        AddNoteActivity.this, NOTIFICATION_CHANNEL_ID)
-                        .setContentTitle("Title")
-                        .setContentText("Body")
-                        .setSmallIcon(R.drawable.ic_launcher_foreground)
-                        .setAutoCancel(true)
-                        .setPriority(NotificationCompat.PRIORITY_MAX) //for old versions
-                        .setChannelId(NOTIFICATION_CHANNEL_ID);//for old versions
-
-                scheduleNotification(builder.build(), delay);
+           scheduleNotification(builder.build(), delay);
 
 
-
-                Intent main = new Intent(AddNoteActivity.this, ListActivity.class)
-                        .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(main);
-                break;
-        }
+           Intent main = new Intent(AddNoteActivity.this, ListActivity.class)
+                   .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+           startActivity(main);
+       }
     }
-    private void scheduleNotification(Notification notification, long delay){
-            Intent notificationIntent = new Intent(this, NotificationUtil.class);
-            notificationIntent.putExtra(NotificationUtil.NOTIFICATION_ID, 1);//unique id
-            notificationIntent.putExtra(NotificationUtil.NOTIFICATION, notification);
-            PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-            long futureInMillis = SystemClock.elapsedRealtime() + delay;
-            AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-            alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureInMillis, pendingIntent);
-        }
+    private void scheduleNotification(Notification notification, long delay) {
+        Intent notificationIntent = new Intent(this, NotificationUtil.class);
+        notificationIntent.putExtra(NotificationUtil.NOTIFICATION_ID, 1);//unique id
+        notificationIntent.putExtra(NotificationUtil.NOTIFICATION, notification);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        long futureInMillis = SystemClock.elapsedRealtime() + delay;
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureInMillis, pendingIntent);
     }
+}
